d9edf3db37588fa37d64c009cdcbc074afdcc86d
Keith Suderman <suderman@jhu.edu>
2023-08-15 14:37:17 -0400

    Add data from test re-run

--- variants/rules/generate_rules.sh
+++ variants/rules/generate_rules.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-for cores in 4 8 16 32 ; do
-    mem=$(($cores * 2))
-    name="$cores"x"$mem".yml
-    render_template.py -t rules-template.yml.j2 cores=$cores mem=$mem > $name
-    echo "Wrote $name"
-done
+# for cores in 4 8 16 32 ; do
+#     mem=$(($cores * 2))
+#     name="$cores"x"$mem".yml
+#     render_template.py -t rules-template.yml.j2 cores=$cores mem=$mem > $name
+#     echo "Wrote $name"
+# done
 for cores in 4 8 ; do
     for mem in 8 16 32 64 ; do
         name="$cores"x"$mem".yml
@@ -13,5 +13,16 @@ for cores in 4 8 ; do
         echo "Wrote $name"
     done
 done
-
+cores=16
+for mem in 16 32 64 ; do
+    name="$cores"x"$mem".yml
+    render_template.py -t rules-template.yml.j2 cores=$cores mem=$mem > $name
+    echo "Wrote $name"
+done
+cores=32
+for mem in 32 64 ; do
+    name="$cores"x"$mem".yml
+    render_template.py -t rules-template.yml.j2 cores=$cores mem=$mem > $name
+    echo "Wrote $name"
+done
 
