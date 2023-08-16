c849dc0fc02a602eed81a2da4f60821f9025f116
Keith Suderman <suderman@jhu.edu>
2023-08-16 11:50:50 -0400

    Resolve conflicts

--- variants/setup.sh
+++ variants/setup.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-cloud=${1:-gkm}
+cloud=${1:-variant}
 
 #namespace=$(kubectl get namespace | grep -v default | grep -v kube | grep Active)
 #ip=$(kubectl get svc -n $namespace | grep nginx | awk '{print $4}')
 #abm config url $cloud http://$ip:8000/galaxy/
 
-#abm $cloud workflow import variant
+abm $cloud workflow import variant
 
-#history=$(abm $cloud history create "Input Data" | jq -r .id)
+abm $cloud history create "Input Data"
 for dataset in forward reverse ; do
 	abm $cloud dataset import --history "Input Data" --name $dataset err3485802.$dataset
 done
